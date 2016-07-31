f39f2bf85164f24c1d900cb3c2e458b66ecbd134
eliseoeric <eric.eliseo@gmail.com>
2016-07-30 23:18:08 -0400

    Updates the special section on the frontpage https://app.asana.com/0/41436221178687/161771018373146

---
+++ wp-content/themes/waves/template-parts/frontpage/specials.php
@@ -0,0 +1,43 @@
+<section class="waves-section frontpage-menu"  data-background="solid #fbf7ec">
+	<!-- Start Image Showcase -->
+	<div class="wrapper">
+	
+		<div class="container">
+			<div class="row">
+				<div class="twelve column pd-b-15 pd-t-15 text-center">
+					<h2>Dinner at Woody's Waterfront</h2>
+					<p>Casual waterfront dining, in the traditions of Old Florida</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="four columns pd-l-10 pd-r-10 jazz-img">
+				<?php waterfront_image_overlay( get_template_directory_uri() . '/img/burgers.jpg', "Woody's Burgers - So close to the water your burger gets wet", "Woody's Burgers" );  ?>
+					<div class="jazz-img__caption">
+						<h5>Woody's Burgers</h5>
+						<p>1/2 lb. ground chuck cooked the way you like it. Includes lettuce, tomato & slaw. Or try our <strong>Ultimate Double Burger</strong></p>
+						<p><a href="#" class="button">View Menu</a></p>
+					</div>
+				</div>
+
+				<div class="four columns pd-l-10 pd-r-10 jazz-img">
+				<?php waterfront_image_overlay( get_template_directory_uri() . '/img/wings.jpg', "World famous Woody's wings - nothing else like them!", "Woody's Wings" );  ?>
+					<div class="jazz-img__caption">
+						<h5>Woody's Wing Basket</h5>
+						<p>Try our world famous wings, plain, spicy or nuclear - served with ranch dressing.</p>
+						<p><a href="#" class="button">View Menu</a></p>
+					</div>
+				</div>
+
+				<div class="four columns pd-l-10 pd-r-10 jazz-img">
+					<?php waterfront_image_overlay( get_template_directory_uri() . '/img/scallops.jpg', "Generous Portion of Bay Scallops, blackened with tomatoes and lots of garlic", "Woody's Large House Salad" );  ?>
+					<div class="jazz-img__caption">
+						<h5>Large House Salads</h5>
+						<p>Cool off with our Bay Scallop topped House Salad. Scallops served blackened with tomatoes and lots of garlic.</p>
+						<p><a href="#" class="button">View Menu</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Image Showcase -->
+</section><!-- #post-## -->
