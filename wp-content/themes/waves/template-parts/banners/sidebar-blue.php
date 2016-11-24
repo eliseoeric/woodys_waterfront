d185f107930940d5cb4bedd80e09cec5b9956a11
eliseoeric <eric.eliseo@gmail.com>
2016-11-23 23:55:45 -0500

    Update the sidebar-blue banner image

--- wp-content/themes/waves/template-parts/banners/sidebar-blue.php
+++ wp-content/themes/waves/template-parts/banners/sidebar-blue.php
@@ -1,4 +1,4 @@
-<section class="waves-section menu-tabs" data-top="0 .5 .5 0 .75 0 1 .2" data-bottom="0 .2 .5 0 .5 .6 1 .6" data-background="image http://localhost/woodys_waterfront/wp-content/uploads/2016/03/entrace_1_bg.png">
+<section class="waves-section menu-tabs" data-top="0 .5 .5 0 .75 0 1 .2" data-bottom="0 .2 .5 0 .5 .6 1 .6" data-background="image <?php echo get_stylesheet_directory_uri() . '/img/entrace_1_bg.png'; ?>">
 	<div class="container">
 		<div class="ten columns offset-by-one">
 			<?php get_template_part( 'sidebar' ); ?>
